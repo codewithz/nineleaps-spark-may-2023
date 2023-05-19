8b8a99c27818ebfb3484b7c7dc36868872f88f95
Code With Z <71726246+codewithz@users.noreply.github.com>
2023-05-19 17:14:04 +0530

    Update snippets.py

--- snippets.py
+++ snippets.py
@@ -58,3 +58,35 @@ schema= StructType([
 
 # https://spark.apache.org/docs/latest/api/python/reference/pyspark.sql/functions.html
 
+# ---------- San Fransisco Fire Calls Case Study ------------------------
+
+fire_schema = StructType([StructField('CallNumber', IntegerType(), True),
+ StructField('UnitID', StringType(), True),
+ StructField('IncidentNumber', IntegerType(), True),
+ StructField('CallType', StringType(), True),
+ StructField('CallDate', StringType(), True),
+ StructField('WatchDate', StringType(), True),
+ StructField('CallFinalDisposition', StringType(), True),
+ StructField('AvailableDtTm', StringType(), True),
+ StructField('Address', StringType(), True),
+ StructField('City', StringType(), True),
+ StructField('Zipcode', IntegerType(), True),
+ StructField('Battalion', StringType(), True),
+ StructField('StationArea', StringType(), True),
+ StructField('Box', StringType(), True),
+ StructField('OriginalPriority', StringType(), True),
+ StructField('Priority', StringType(), True),
+ StructField('FinalPriority', IntegerType(), True),
+ StructField('ALSUnit', BooleanType(), True),
+ StructField('CallTypeGroup', StringType(), True),
+ StructField('NumAlarms', IntegerType(), True),
+ StructField('UnitType', StringType(), True),
+ StructField('UnitSequenceInCallDispatch', IntegerType(), True),
+ StructField('FirePreventionDistrict', StringType(), True),
+ StructField('SupervisorDistrict', StringType(), True),
+ StructField('Neighborhood', StringType(), True),
+ StructField('Location', StringType(), True),
+ StructField('RowID', StringType(), True),
+ StructField('Delay', FloatType(), True)])
+
+
