23ee95b544b67d4a2791518daf2c77a0507e481b
Code With Z <71726246+codewithz@users.noreply.github.com>
2023-05-22 15:04:38 +0530

    Update snippets.sql

--- snippets.sql
+++ snippets.sql
@@ -1,1 +1,15 @@
-
+CREATE EXTERNAL TABLE transactions
+(
+    txnno INT,
+    txndate STRING,
+    custno INT,
+    amount DOUBLE,
+    product STRING,
+    category STRING,
+    city STRING,
+    state STRING,
+    payment_mode STRING
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+LOCATION '/user/splab298046/datasets/transactions';
